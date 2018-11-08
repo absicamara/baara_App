dfb483b5518e61c2b19095d55e1ad9a3a877aab5
slash0593 <absicamara@gmail.com>
2018-11-08 23:16:09 +0000

    set up splash screen

--- app/src/main/java/net/geeksh/baaraapp/Splash.java
+++ app/src/main/java/net/geeksh/baaraapp/Splash.java
@@ -1,13 +1,38 @@
 package net.geeksh.baaraapp;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.VideoView;
 
 public class Splash extends AppCompatActivity {
+
+    VideoView splashVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
+
+        splashVideo = (VideoView) findViewById(R.id.splashVD);
+//        getSupportActionBar().hide();
+
+        Uri vd = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.splashvideo);
+        splashVideo.setVideoURI(vd);
+
+        splashVideo.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                startActivity(new Intent(Splash.this, LoginActivity.class));
+                finish();
+            }
+        });
+        splashVideo.setZOrderOnTop(true);
+        splashVideo.start();
     }
 }
