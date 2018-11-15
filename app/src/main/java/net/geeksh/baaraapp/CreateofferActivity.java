4f0bb0bceb5d4c135a3dae36a0ccb7d040c20142
slash0593 <absicamara@gmail.com>
2018-11-15 02:49:25 +0000

    save offer to firebase

--- app/src/main/java/net/geeksh/baaraapp/CreateofferActivity.java
+++ app/src/main/java/net/geeksh/baaraapp/CreateofferActivity.java
@@ -1,13 +1,81 @@
 package net.geeksh.baaraapp;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class CreateofferActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class CreateofferActivity extends AppCompatActivity implements View.OnClickListener {
+
+    DatabaseReference databaseReference;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+
+    EditText editTextJobTitle, editTextjobDescription, editTextjobRequirement, editTextStartDate, editTextEndDate, editTextCity, editTextCountry;
+
+    Button buttonCreateOffer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createoffer);
+
+        editTextJobTitle = (EditText) findViewById(R.id.editTextJobTitle);
+        editTextjobDescription = (EditText) findViewById(R.id.editTextJobDescription);
+        editTextjobRequirement = (EditText) findViewById(R.id.editTextJobRequirement);
+        editTextStartDate = (EditText) findViewById(R.id.editTextofferStartDate);
+        editTextEndDate = (EditText) findViewById(R.id.editTextofferEndDate);
+        editTextCity = (EditText) findViewById(R.id.editTextOfferCity);
+        editTextCountry = (EditText) findViewById(R.id.editTextOfferCountry);
+        buttonCreateOffer = (Button) findViewById(R.id.buttonCreateOffer2);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        buttonCreateOffer.setOnClickListener(this);
+
+
+//        databaseReference = FirebaseDatabase.getInstance().getReference();
+    }
+
+    public void createOffer(){
+
+        String jobTitle = editTextJobTitle.getText().toString().trim();
+        String desc = editTextjobDescription.getText().toString().trim();
+        String req = editTextjobRequirement.getText().toString().trim();
+        String startDate = editTextStartDate.getText().toString().trim();
+        String endDate = editTextEndDate.getText().toString().trim();
+        String city = editTextCity.getText().toString().trim();
+        String country = editTextCountry.getText().toString().trim();
+
+        Offer offer = new Offer(jobTitle, req,city,country,startDate,endDate,desc);
+        user = firebaseAuth.getCurrentUser();
+
+//        databaseReference = databaseReference.getRoot();
+        databaseReference.child(user.getUid()+"/Offer/"+Offer.getOfferId()).setValue(offer);
+//        Toast.makeText(this, "done", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == buttonCreateOffer){
+
+            createOffer();
+        }
     }
 }
--- app/src/main/java/net/geeksh/baaraapp/Offer.java
+++ app/src/main/java/net/geeksh/baaraapp/Offer.java
@@ -1,20 +1,54 @@
 package net.geeksh.baaraapp;
 
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.os.Bundle;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.Date;
 
-public class Offer {
+public class Offer  {
 
-    private Double jobId;
-    private String company;
-    private String requirement;
-    private String city;
-    private String country;
-    private Date date;
-    private Date endDate;
-    private String description;
-    private double latitude;
-    private double longitude;
-    private boolean expired;
+    DatabaseReference databaseRef;
+    FirebaseDatabase firebaseDatabase;
+
+
+
+    public static int offerId = 0;
+    public String jobTitle;
+    public String company;
+    public String requirement;
+    public String city;
+    public String country;
+    public String startDate;
+    public String endDate;
+    public String description;
+    public double latitude;
+    public double longitude;
+    public boolean expired;
+
+
+    public Offer(String jobTitle, String requirement, String city, String country, String startDate, String endDate, String description) {
+        this.jobTitle = jobTitle;
+        this.requirement = requirement;
+        this.city = city;
+        this.country = country;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+        offerId++;
+    }
+
+
+    public static String getOfferId(){
+        String id =  Integer.toString(offerId);
+        return id;
+    }
+
 
 
 }
